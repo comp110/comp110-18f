5b46e4da34f2d657653166537f9d7d43143b81d7
Kris Jordan <krisjordan@gmail.com>
2018-09-18 11:01:08 -0400

    Lec 6 setup

---
+++ src/06-arrays/array-functions.ts
@@ -0,0 +1,26 @@
+/**
+ * Fill an array of length n with zeros.
+ * @param n - the number of 0s
+ */
+export let fillZeros = (n: number): number[] => {
+    return [];
+};
+
+/**
+ * Fill an array of numbers from low to high, inclusive.
+ * 
+ * @param low 
+ * @param high 
+ */
+export let fillRange = (low: number, high: number): number[] => {
+    return [];
+};
+
+/**
+ * Given a number array, compute their sum.
+ * 
+ * @param a 
+ */
+export let sum = (a: number[]): number => {
+    return 0;
+};
---
+++ src/06-arrays/array-practice-app.ts
@@ -0,0 +1,10 @@
+import { testNumber, testArray } from "./test-util";
+
+import { fillRange } from "./array-functions";
+
+export let main = async () => {
+    // TODO: Add Test Cases
+};
+
+
+main();
