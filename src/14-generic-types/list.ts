7a233cc3c6e717c4ffdebbea4ab2c2b2d1b932be
Kris Jordan <krisjordan@gmail.com>
2018-10-25 11:51:48 -0400

    Add lecture 14 materials.

---
+++ src/14-generic-types/list.ts
@@ -0,0 +1,50 @@
+/* Data Structure: Linked List */
+
+export class Node<T> {
+    data: T;
+    next: Node<T> = null;
+}
+
+/* Constructor */
+
+export let cons = <T> (data: T, next: Node<T>): Node<T> => {
+    let n = new Node<T>();
+    n.data = data;
+    n.next = next;
+    return n;
+};
+
+/* Selectors */
+
+export let first = <T> (n: Node<T>): T => {
+    return n.data;
+};
+
+export let rest = <T> (n: Node<T>): Node<T> => {
+    return n.next;
+};
+
+/* Helper Functions */
+
+export let toString = <T> (n: Node<T>): string => {
+    if (n === null) {
+        return "NULL";
+    } else {
+        return first(n) + " → " + toString(rest(n));
+    }
+};
+
+export let listify = <T> (items: T[]): Node<T> => {
+    // A helper function *inside* another function.
+    // It is only defined inside of the listify function!
+    // We'll discuss nested function definitions like this,
+    // called closures, after this unit on recursion.
+    let builder = (i: number): Node<T> => {
+        if (i === items.length) {
+            return null;
+        } else {
+            return cons(items[i], builder(i + 1));
+        }
+    };
+    return builder(0);
+};
