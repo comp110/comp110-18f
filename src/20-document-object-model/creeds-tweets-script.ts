618074418081d25ea084d3903fe3cd26343c8c01
Kris Jordan <krisjordan@gmail.com>
2018-11-29 12:17:23 -0500

    Add lecture 20 starter materials

--- src/20-document-object-model/creeds-tweets-script.ts
+++ src/20-document-object-model/creeds-tweets-script.ts
@@ -1,9 +1,11 @@
-// TODO
+// TODO: Uncomment these variables as needed.
+// let text = document.getElementById("text") as HTMLInputElement;
+// let characters = document.getElementById("characters") as HTMLSpanElement;
+// let form = document.getElementById("form") as HTMLFormElement;
+// let tweets = document.getElementById("tweets") as HTMLDivElement;
 
 export let main = async () => {
-    
-    // TODO
-
+    // TODO: Add Event Handlers
 };
 
 main();
